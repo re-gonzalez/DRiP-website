921654741ea9492e8fb1494d4911824f9a642576
re-gonzalez <156967903+re-gonzalez@users.noreply.github.com>
2024-02-25 14:41:58 -0800

    Added Results Table

--- scripts/main.js
+++ scripts/main.js
@@ -5,24 +5,52 @@ const twoDecimalUSD = new Intl.NumberFormat("en-US", {
 });
 
 const initPrincipalElem = document.querySelector('.js-initial-principal-input');
+const monthlyContributionElem = document.querySelector('.js-monthlyc-input');
 const apyPercentElem = document.querySelector('.js-apy-input');
 const totalYearsElem = document.querySelector('.js-years-input');
 const compFrequElem = document.querySelector('.js-frequency-select');
 
-function calculateFinalValue() {
-  const initPrincipal = initPrincipalElem.value;
-  const apyPercent = apyPercentElem.value;
-  const totalYears = totalYearsElem.value;
+function calculateCompoundInterest() {
+  const initPrincipal = Number(initPrincipalElem.value);
+  const monthlyContribution = Number(monthlyContributionElem.value) || 0;
+  const apyPercent = Number(apyPercentElem.value);
+  const totalYears = Number(totalYearsElem.value);
   const compFreq = Number(compFrequElem.value);
   
   let i = 0;
-  let currentPrincipal = initPrincipal;
+  let currentPrincipal = [initPrincipal];
   while(i<(totalYears*compFreq)) {
-    currentPrincipal = currentPrincipal*(1+(apyPercent/(100*compFreq)));
+    currentPrincipal[i+1] = currentPrincipal[i]*(1+(apyPercent/(100*compFreq))) + monthlyContribution*(12/compFreq);
     i++;
   }
   
-  outString = twoDecimalUSD.format(currentPrincipal);
+  outString = twoDecimalUSD.format(currentPrincipal[currentPrincipal.length - 1]);
 
   document.querySelector('.js-final-value-text').innerHTML = outString;
+
+  printCompoundInterestColumn(currentPrincipal,compFreq);
+  printYearsColumn(totalYears);
+}
+
+function printCompoundInterestColumn(array, freq) {
+  const arrayLength = array.length;
+  outString = '';
+  let i = 0;
+  while(i<arrayLength) {
+    if(i % freq === 0) {
+      outString = outString + `${twoDecimalUSD.format(array[i])}<br><br>`;
+    }
+    i++;
+  }
+  document.querySelector('.js-output-text-principal').innerHTML = outString;
+}
+
+function printYearsColumn(years) {
+  outString = '';
+  let i = 0;
+  while(i<years+1) {
+    outString = outString + `Year ${i}<br><br>`;
+    i++;
+  }
+  document.querySelector('.js-output-text-years').innerHTML = outString;
 }
