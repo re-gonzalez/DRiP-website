5eb52e79af8e6fb7e6372e81ff55027ce094acfe
re-gonzalez <156967903+re-gonzalez@users.noreply.github.com>
2024-03-07 00:27:20 -0800

    Header Update
    Updated all styling on the header and created a new JavaScript file dedicated to creating the header html elements. All pages will load this script to ensure all pages have an identical header.

---
+++ scripts/header.js
@@ -0,0 +1,19 @@
+document.querySelector('.js-header-div').innerHTML = `
+  <button class="header-button" onclick="
+    document.location='articles.html'
+  ">
+    Articles
+  </button>
+
+  <button class="header-button" onclick="
+    document.location='index.html'
+  ">
+    Calculator Page
+  </button>
+
+  <button class="header-button" onclick="
+    document.location='stock-picks.html'
+  ">
+    Monthly Stock Picks
+  </button>
+`;
