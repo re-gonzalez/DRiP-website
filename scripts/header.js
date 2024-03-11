adf3285f25273ff2413b6967e3635bcc64a0d05f
re-gonzalez <156967903+re-gonzalez@users.noreply.github.com>
2024-03-10 20:42:46 -0700

    Scrollable Header
    Added overflow capability to the header and allowed it to be scrollable horizontally.

--- scripts/header.js
+++ scripts/header.js
@@ -2,45 +2,47 @@ document.querySelector('.js-header-div').innerHTML = `
   <div class="header-logo-div">
   </div>
   
-  <button class="header-button" onclick="
-    document.location='index.html';
-  ">
-    Home Page
-  </button>  
+  <div class="header-buttons-div">
+    <button class="header-button" onclick="
+      document.location='index.html';
+    ">
+      Home Page
+    </button>  
 
-  <button class="header-button" onclick="
-    document.location='articles.html';
-  ">
-    Articles
-  </button>
+    <button class="header-button" onclick="
+      document.location='articles.html';
+    ">
+      Articles
+    </button>
 
-  <button class="header-button" onclick="
-    document.location='investment-calc.html';
-  ">
-    Investment Calculators
-  </button>
+    <button class="header-button" onclick="
+      document.location='investment-calc.html';
+    ">
+      Investment Calculators
+    </button>
 
-  <button class="header-button" onclick="
-    document.location='tax-calc.html';
-  ">
-    Tax Calculator
-  </button>
+    <button class="header-button" onclick="
+      document.location='tax-calc.html';
+    ">
+      Tax Calculator
+    </button>
 
-  <button class="header-button" onclick="
-    document.location='my-portfolio.html';
-  ">
-    My Portfolio
-  </button>
+    <button class="header-button" onclick="
+      document.location='my-portfolio.html';
+    ">
+      My Portfolio
+    </button>
 
-  <button class="header-button" onclick="
-    document.location='disclaimer.html';
-  ">
-    Disclaimer
-  </button>
+    <button class="header-button" onclick="
+      document.location='disclaimer.html';
+    ">
+      Disclaimer
+    </button>
 
-  <button class="header-button" onclick="
-    document.location='donate.html';
-  ">
-    Donate
-  </button>
+    <button class="header-button" onclick="
+      document.location='donate.html';
+    ">
+      Donate
+    </button>
+  </div>
 `;
