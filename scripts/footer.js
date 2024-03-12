6eac5c265910d33f46adfb93f3304319831ca5ef
re-gonzalez <156967903+re-gonzalez@users.noreply.github.com>
2024-03-11 21:49:16 -0700

    Standard Footer
    Added standardized footer to every page.

---
+++ scripts/footer.js
@@ -0,0 +1,16 @@
+document.querySelector('.js-footer-div').innerHTML = `
+<div class="footer-logo-div">
+  <img class="footer-logo-img" src="images/ez-money-logo.png">
+</div>
+<div class="footer-links-div">
+  <p onclick="document.location='index.html';">Home Page</p>
+  <p onclick="document.location='disclaimer.html';">Disclaimer</p>
+  <p>Privacy Policy</p>
+</div>
+
+<div class="footer-links-div">
+  <p onclick="document.location='donate.html';">Donate</p>
+  <p>Contact Us</p>
+  <p>Newsletter</p>
+</div>
+`;
